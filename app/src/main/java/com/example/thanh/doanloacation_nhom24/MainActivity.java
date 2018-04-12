90836d9eccf4be4fb5b1933f6cd5e60c27f3178d
Thanh <thanhvantran0@gmail.com>
2018-04-12 14:44:54 +0700

    Cập nhật toolbar

--- app/src/main/java/com/example/thanh/doanloacation_nhom24/MainActivity.java
+++ app/src/main/java/com/example/thanh/doanloacation_nhom24/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.thanh.doanloacation_nhom24;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+//Activity Trang chu
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolBarTrangChu);
+        setSupportActionBar(toolbar);
+
+        if(getSupportActionBar()!=null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_trangchu,menu);
+
+        MenuItem searchview = menu.findItem(R.id.itSearch);
+        SearchView search = (SearchView) searchview.getActionView();
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this,query, Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Bắt sự kiện click item menu
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
