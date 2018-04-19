ccfe4e3bd360000596432230d53d20de71929ed4
Thanh <thanhvantran0@gmail.com>
2018-04-19 20:52:58 +0700

    Cập nhật Nav drawer

--- app/src/main/java/com/example/thanh/doanlocation_nhom24/Map_Activity.java
+++ app/src/main/java/com/example/thanh/doanlocation_nhom24/Map_Activity.java
@@ -1,13 +1,25 @@
 package com.example.thanh.doanlocation_nhom24;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Map_Activity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_map);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+        //getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
